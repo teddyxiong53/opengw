bfc60b2dd0d8fa1ac121f9ad47d7694228112fd3
pengwang <969753572@qq.com>
2021-05-12 14:29:39 +0800

    修改：1、阿里云调用服务接口通过解析method中things名字来判断自定义命令的名字

--- report/mqttAliyun/mqttAliyunService.go
+++ report/mqttAliyun/mqttAliyunService.go
@@ -14,23 +14,19 @@ func (r *ReportServiceParamAliyunTemplate) ProcessInvokeThingsService() {
 		case reqFrame := <-r.InvokeThingsServiceRequestFrameChan:
 			{
 				setting.Logger.Debugf("reqFrame %v", reqFrame)
+				methodParam := strings.Split(reqFrame.Method, ".")
+				if len(methodParam) != 3 {
+					continue
+				}
 				name := reqFrame.Params["Name"].(string)
 				for _, n := range r.NodeList {
 					if name == n.Param.DeviceName {
 						cmd := device.CommunicationCmdTemplate{}
-						if strings.Contains(reqFrame.Method, "RelayOpen") {
-							cmd.CollInterfaceName = n.CollInterfaceName
-							cmd.DeviceName = n.Name
-							cmd.FunName = "RelayOpen"
-							paramStr, _ := json.Marshal(reqFrame.Params)
-							cmd.FunPara = string(paramStr)
-						} else if strings.Contains(reqFrame.Method, "RelayClose") {
-							cmd.CollInterfaceName = n.CollInterfaceName
-							cmd.DeviceName = n.Name
-							cmd.FunName = "RelayClose"
-							paramStr, _ := json.Marshal(reqFrame.Params)
-							cmd.FunPara = string(paramStr)
-						}
+						cmd.CollInterfaceName = n.CollInterfaceName
+						cmd.DeviceName = n.Name
+						cmd.FunName = methodParam[2]
+						paramStr, _ := json.Marshal(reqFrame.Params)
+						cmd.FunPara = string(paramStr)
 
 						ack := MQTTAliyunInvokeThingsServiceAckTemplate{}
 						if len(device.CommunicationManage) > 0 {
